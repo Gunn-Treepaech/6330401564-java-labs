018654722b55b3884affb0fcecf84e377c877749
Gunn-Treepaech <treepaech@kkumail.com>
2021-01-02 12:49:11 +0700

    add lab2

--- labs/src/treeechan/treepaech/lab2/CamelCaseNaming.java
+++ labs/src/treeechan/treepaech/lab2/CamelCaseNaming.java
@@ -2,19 +2,22 @@ package treeechan.treepaech.lab2;
 
 public class CamelCaseNaming {
     public static void main(String[] args){
-       String msgfi,bigmsgfi,msgse,bigmsgse,lowtxt1,lowtxt2;
-       String newfi,newse;
+
+       String[] news = new String[3];
+       String[] lowtxt = new String[3],msg = new String[3], big = new String[3];
 
        if (args.length == 2) {
-           lowtxt1 = args[0].toLowerCase();   // Make all letters lowercase
-           msgfi = lowtxt1.substring(0, 1);   // Separate only the first from the text
-           bigmsgfi = msgfi.toUpperCase();   // Capitalize the text
-           newfi = bigmsgfi + lowtxt1.substring(1);  // Put back together
-           lowtxt2 = args[1].toLowerCase();
-           msgse = lowtxt2.substring(0, 1);
-           bigmsgse = msgse.toUpperCase();
-           newse = bigmsgse + lowtxt2.substring(1);
-           System.out.println(newfi + newse);   // Bringing the two messages together
+
+           for (int i=0;i<2;i++){
+               lowtxt[i] = args[i].toLowerCase();        // Make all letters lowercase
+               msg[i] = lowtxt[i].substring(0, 1);       // Separate only the first from the text
+               big[i] = msg[i].toUpperCase();            // Capitalize the text
+               news[i] = big[i] + lowtxt[i].substring(1);  // Put back together
+           }
+
+           System.out.println("The first word is "+args[0]);
+           System.out.println("The second word is "+args[1]);
+           System.out.println("The concatenate with camel case is "+(news[0] + news[1]));// Bringing the two messages together
        }
        else {
            System.err.println("CamelCaseNaming : <First word> <Second word>");
