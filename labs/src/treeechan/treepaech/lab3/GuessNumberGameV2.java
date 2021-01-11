7e2bf20381c93a133f21bdeb5405cce776558c9e
Gunn-Treepaech <treepaech@kkumail.com>
2021-01-11 14:09:56 +0700

    add labs

--- labs/src/treeechan/treepaech/lab3/GuessNumberGameV2.java
+++ labs/src/treeechan/treepaech/lab3/GuessNumberGameV2.java
@@ -1,3 +1,16 @@
+/**
+ * This  GuessNumberGameV2 program receives a number of numbers.
+ * ts output format is " If the user's numbers are less than random numbers <Please type a Lower number!>
+ *                       If the user's numbers are greater than random numbers <Please type a higher number!>
+ *                       If the user's numbers are equal to random numbers <Congratulations!  That’s correct> "
+ *
+ * Author:Treepaech Treechan
+ * ID: 633040156-4
+ * Sec: 1
+ * Date:January 10, 2021
+ *
+ **/
+
 package treeechan.treepaech.lab3;
 
 import java.util.Scanner;
@@ -8,21 +21,19 @@ public class GuessNumberGameV2 {
     public static void main(String[] args){
         genAnswer();
         playGame();
-
     }
     public static void genAnswer(){
         correctNum = minNum + (int) (Math.random() * ((maxNum - minNum) + 1));
     }
     public static void playGame(){
         int numLoop = 0,triesNum = 2;
-        int userNumber;
         int userNumInt;
         Scanner getNum = new Scanner(System.in);
         String userNum;
-        System.out.print("Please enter a guess (1—10):");
-        userNum = getNum.nextLine();
-        userNumInt = Integer.parseInt(userNum);
         while (numLoop <= triesNum){
+            System.out.print("Please enter a guess (1—10):");
+            userNum = getNum.nextLine();
+            userNumInt = Integer.parseInt(userNum);
             if (userNumInt != correctNum){
                 if (userNumInt < correctNum){
                     System.out.println("Please type a Lower number! Number of remaining tries:" + (triesNum - numLoop));
