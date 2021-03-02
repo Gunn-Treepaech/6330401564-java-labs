7ad8d0eac14edaaaee6b0bc87ff9dc55c5881bf4
Gunn-Treepaech <treepaech@kkumail.com>
2021-03-02 23:56:08 +0700

    add lab7

--- labs/src/treeechan/treepaech/lab7/GuessNumberGameVer4.java
+++ labs/src/treeechan/treepaech/lab7/GuessNumberGameVer4.java
@@ -3,16 +3,19 @@ package treeechan.treepaech.lab7;
 import treeechan.treepaech.lab5.GuessNumberGameVer3;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 public class GuessNumberGameVer4 extends GuessNumberGameVer3 {
-    static ArrayList<GuessNumberGameVer3> games = new ArrayList<GuessNumberGameVer3>(0);
+    static ArrayList<GuessNumberGameVer4> games = new ArrayList<GuessNumberGameVer4>(0);
+
+    public GuessNumberGameVer4(int minNum, int maxNum, int maxTries) {
+        super(minNum, maxNum, maxTries);
+    }
 
     public static void initGamesList(){
-        games.add(new GuessNumberGameVer3(1,10,3));
-        games.add(new GuessNumberGameVer3(1,10,5));
-        games.add(new GuessNumberGameVer3(1,5,5));
+        games.add(new GuessNumberGameVer4(1,10,3));
+        games.add(new GuessNumberGameVer4(1,10,5));
+        games.add(new GuessNumberGameVer4(1,5,5));
 
         System.out.println("Unsorted");
         for (GuessNumberGameVer3 game: games){
@@ -22,7 +25,7 @@ public class GuessNumberGameVer4 extends GuessNumberGameVer3 {
 
     public static void testComparingMaxTries(){
         initGamesList();
-
+        Collections.sort(games, new SortByMaxTries());
 
         System.out.println("\nSorted by max tries in descending oder");
         for (GuessNumberGameVer3 game : games){
--- labs/src/treeechan/treepaech/lab7/SortByMaxTries.java
+++ labs/src/treeechan/treepaech/lab7/SortByMaxTries.java
@@ -1,8 +1,12 @@
 package treeechan.treepaech.lab7;
 
-public class SortByMaxTries {
+import treeechan.treepaech.lab5.GuessNumberGameVer3;
+import java.util.Comparator;
 
-    public SortByMaxTries(){
-
+class SortByMaxTries implements Comparator<GuessNumberGameVer3> {
+    public int compare(GuessNumberGameVer3 a, GuessNumberGameVer3 b){
+        return b.getMaxTries() - a.getMaxTries();
     }
+
+
 }
