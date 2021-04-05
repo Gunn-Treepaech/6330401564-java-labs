9dd6e7c4f0ebb2555139ffbdb9b7920c8453cb68
Gunn-Treepaech <treepaech@kkumail.com>
2021-04-05 17:00:35 +0700

    add lab10

---
+++ labs/src/treeechan/treepaech/lab10/PlayerFormV7.java
@@ -0,0 +1,57 @@
+package treeechan.treepaech.lab10;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.util.ArrayList;
+
+public class PlayerFormV7 extends PlayerFormV6 implements ListSelectionListener {
+
+    public PlayerFormV7(String titleName) {
+        super(titleName);
+    }
+
+    @Override
+    public void addComponents() {
+        super.addComponents();
+        gameList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+    }
+
+    @Override
+    public void addListener(){
+       gameList.addListSelectionListener(this);
+    }
+
+    public static void createAndShowGUI() {
+        PlayerFormV7 pfv7 = new PlayerFormV7("Player Form V7");
+        pfv7.addComponents();
+        pfv7.setFrameFeatures();
+        pfv7.addListener();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+        StringBuffer msg = new StringBuffer();
+        boolean isAdjusting = e.getValueIsAdjusting();
+        if (!isAdjusting) {
+            ArrayList<String> selectedNames = (ArrayList<String>) gameList.getSelectedValuesList();
+            int numSelected = selectedNames.size();
+            for (int i = 0; i < numSelected; i++) {
+                    msg.append(selectedNames.get(i));
+                    if(i != numSelected - 1){
+                        msg.append(", ");
+                    }
+            }
+            JOptionPane.showMessageDialog(null, msg.toString());
+        }
+    }
+
+}
